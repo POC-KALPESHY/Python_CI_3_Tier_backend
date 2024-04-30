9e3aca689dce1eeb4142608afb97841983760dee
onkarko <onkarko@cybage.com>
2024-04-30 15:36:27 +0530

    adding the python project

---
+++ src/routes/routes.py
@@ -0,0 +1,27 @@
+from flask import request
+from flask import (
+    jsonify,
+    make_response
+)
+from src.___init__ import app
+# from src.utils.db_function import get_cursor,get_object
+
+
+
+@app.route("/user",methods = ['GET'])
+def hello():
+    # cursor, connection = get_cursor()
+    print("Inside To get basic set up details")
+    try:
+        if request.method == 'GET':
+            # query = """ select * from api.api_user """
+            # user_details = get_object(
+            #     query=query,
+            #     cursor=cursor
+            # )
+            user_details = {
+                "name":"onkar"
+            }
+            return make_response(jsonify(user_details))
+    except Exception as e:
+        print(e)
