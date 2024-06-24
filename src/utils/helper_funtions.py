2dbc99bcdb2064fa1835e046683ee702f20deaa3
onkarko <onkarko@cybage.com>
2024-06-24 14:52:14 +0530

    changes in the helper file

--- src/utils/helper_funtions.py
+++ src/utils/helper_funtions.py
@@ -2,12 +2,21 @@ import os
 import json
 
 
+# def load_db_config():
+#     """TODO need to implement the basedir"""
+#
+#     json_file_path = "src/resources/db_config.json"
+#
+#     with open(json_file_path, 'r') as fr:
+#         content = json.loads(fr.read())
+#
+#     return content
+
+
+
+
 def load_db_config():
-    """TODO need to implement the basedir"""
-
-    json_file_path = "src/resources/db_config.json"
-
+    json_file_path = os.path.join(os.path.dirname(__file__), '..', 'resources', 'db_config.json')
     with open(json_file_path, 'r') as fr:
-        content = json.loads(fr.read())
-
+        content = json.load(fr)  # Use json.load() to parse JSON correctly
     return content
